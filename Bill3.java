d05e7306198f5dd9f12797278faa85a0d1b74165
nitsan tsalka <nitsan.tsalka@post.runi.ac.il>
2025-10-31 10:23:05 +0200

    did ex Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,15 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name1 = "Ron";
+		String name2 = "Lisa";
+		String name3 = "Dan";
+		String name4 = "100";
+		int sum = Integer.parseInt(name4);
+		double sumeach = (double)sum/3; 
+
+		System.out.print("dear " +name3 +", " + name2 + ", and " + name1 +
+		 ": pay " + Math.ceil(sumeach) + " Shekels each" );
+	       
 	}
 }
