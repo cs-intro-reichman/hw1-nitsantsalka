a60e2e06d71096958ad263366236504aa92caff0
Nitsan Tsalka <nitsan.tsalka@post.runi.ac.il>
2025-11-03 21:04:34 +0200

    finished

--- Bill3.java
+++ Bill3.java
@@ -8,7 +8,7 @@ public class Bill3 {
 		int sum = Integer.parseInt(args[3]);
 		double sumeach = Math.ceil((double)sum/3); 
 
-		System.out.print("dear " +name3 +", " + name2 + ", and " + name1 +
+		System.out.print("Dear " +name3 +", " + name2 + ", and " + name1 +
 		 ": pay " + sumeach + " Shekels each" );
 	       
 	}
