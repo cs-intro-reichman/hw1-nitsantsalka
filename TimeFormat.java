a282b85ec65b2ff64b71a4b1c1b265ef64869a6c
Nitsan Tsalka <nitsan.tsalka@post.runi.ac.il>
2025-11-03 21:09:15 +0200

    finished

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,7 +12,7 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		if (hours>12)
+		if (hours>=12)
 		{
 			System.out.print((hours-12) + ":");
 			if(minutes<10)
